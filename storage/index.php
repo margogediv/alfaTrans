2a2863d7576f9312485d1504682bf55b7da019c4
Pavel Zholnerovich <pavel.zholnerovich@yandex.ru>
2020-06-18 10:58:51 +0300

    demo php

---
+++ storage/index.php
@@ -0,0 +1,121 @@
+<?php
+include "header.php";
+?>
+
+<!-- Content -->
+<div class="content">
+    <div class="section screen section-1 banner">
+        <div class="banner-img-storage">
+
+            <img id="banner-img-home" class="photo-style" src="/img/storage/img-content.png?v=1"
+                 style="display: none" alt="">
+            <img class="photo-style" data-type="tablet" src="/img/storage/img-content-tablet.png?v=1"
+                 alt="">
+            <img class="photo-style" data-type="mobile" src="/img/storage/img-content-mobile.png?v=1"
+                 alt="">
+        </div>
+        <div class="banner-text-storage">
+            <div class="text-container-storage">
+                <h1 class="banner-title">
+                    <span class="text-blue-storage">Ответственное хранение</span><br data-type="mobile">
+                </h1>
+                <span class="banner-subtitle">
+                            <p class="subtitle-bold">Склад ответственного хранения<br> ООО «Альфа Транс Безопасность»</p>
+                            предлагает клиентам ответственное хранение товаров от 1 м2 или 1 коробки<br> сроком от 1 дня в Смоленской области.
+                        </span>
+                <button class="btn btn-red modal storage">Отправить заявку</button>
+            </div>
+        </div>
+    </div>
+    <div class="section content-storage">
+        <div class="container">
+            <div class="box-left">
+                <ul class="list-storage">
+                    <li>
+                        <img src="/img/storage/icon/01.svg" alt="">
+                        <div class="text-content">
+                            Мы принимаем на ответственное хранение
+                            <p>грузы любых объёмов, габаритов и сложности.</p>
+                        </div>
+                    </li>
+                </ul>
+            </div>
+            <div class="box-richt">
+                <ul class="list-storage">
+                    <li>
+                        <img src="/img/storage/icon/02.svg" alt="">
+                        <div class="text-content">
+                            Компания осуществляет ответственное хранение товара не только
+                            <span>в крытых помещениях</span>
+                            складов, но и ответственное хранение оборудования <span>на открытой площадке</span> с
+                            применением спецтехники для осуществления погрузо-разгрузочных работ.
+                        </div>
+                    </li>
+                </ul>
+            </div>
+            <div class="box-left">
+                <ul class="list-storage">
+                    <li>
+                        <img src="/img/storage/icon/03.svg" alt="">
+                        <div class="text-content">
+                            Преимуществом ответственного хранения груза является <span>оптимизация</span>
+                            <span>клиентских издержек</span>, так как Вы платите только за фактически занятый
+                            объём. У Вас отпадает необходимость содержания и оборудования
+                            собственного складского помещения, содержания штата сотрудников,
+                            закупки упаковочных материалов, обслуживания спецтехники.
+                        </div>
+                    </li>
+                </ul>
+            </div>
+            <div class="box-richt">
+                <ul class="list-storage">
+                    <li>
+                        <img src="/img/storage/icon/04.svg" alt="">
+                        <div class="text-content">
+                            Передача на ответственное хранение происходит на основании <span>акта</span>
+                            <span>приёма передачи товара</span> на ответственное хранение
+                            (<a class="text-link" href=#>Акт на ответственное хранение</a> образец).
+                        </div>
+                    </li>
+                </ul>
+            </div>
+            <div class="box-left storage">
+                <ul class="list-storage">
+                    <li>
+                        <img src="/img/storage/icon/05.svg" alt="">
+                        <div class="text-content">
+                            Мы гарантирует <span>качественную и количественную</span>
+                            <span>сохранность</span> груза с момента приёма и до момента
+                            списания груза со склада.
+                        </div>
+                    </li>
+                </ul>
+            </div>
+            <div class="box-richt">
+                <ul class="list-storage">
+                    <li>
+                        <img class="img-box" src="/img/storage/icon/06.svg" alt="">
+                        <div class="last-block">
+                            <div class="text-content button">
+                                Отправьте нам <span>заявку</span>, чтобы запросить договор
+                                ответственного хранения и <span>узнать стоимость</span>
+                                ответственного хранения на нашем складе.
+                            </div>
+                            <button class="btn btn-red modal">Отправить заявку</button>
+                        </div>
+                    </li>
+
+                </ul>
+            </div>
+        </div>
+    </div>
+</div>
+</div> <!-- app end -->
+
+<?php
+include "footer.php";
+?>
+
+</body>
+</html>
+
